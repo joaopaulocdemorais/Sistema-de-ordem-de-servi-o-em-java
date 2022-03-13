59de596f882ab3903ba094785bb24a2576592006
João Paulo <joaopaulomorais2@gmail.com>
2022-03-13 15:27:05 -0300

    update class conexão

--- infox/src/br/com/infox/dal/ModuloConexao.java
+++ infox/src/br/com/infox/dal/ModuloConexao.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.com.infox.dal;
 
-/**
- *
- * @author Java
- */
+package br.com.infox.dal;
+import java.sql.*;
+
 public class ModuloConexao {
-    
+   // o metodo abaixo faz a conexao com o banco de dados
+   public static Connection conector(){
+       java.sql.Connection conexao = null; 
+       // a linha abaixo chama o drive de coneção com o banco de dados
+       String driver = "com.mysql.jdbc.Driver";
+       //Armazenando informações referentes ao banco de dados
+       String url = "//127.0.0.1:3306/dbinfox";
+       String user = "root";
+       String password = "Jopacamo@1996";
+       // fazendo a conexão com o banco de dados
+       try {
+           Class.forName(driver);
+           conexao = DriverManeger.getConnection(url, user, password);
+       } catch (Exception e) {
+       }
+   }  
+           
 }
