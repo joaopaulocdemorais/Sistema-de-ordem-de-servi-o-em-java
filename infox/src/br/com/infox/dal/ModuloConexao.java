7fde387a71025c505e62a742306c52eacd496069
Joao Paulo <joaopaulomorais2@gmail.com>
2022-06-21 09:31:27 -0300

    Documentação da classe Conexão

--- infox/src/br/com/infox/dal/ModuloConexao.java
+++ infox/src/br/com/infox/dal/ModuloConexao.java
@@ -1,9 +1,39 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2022 joaop.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 
 package br.com.infox.dal;
 import java.sql.*;
-
+/**
+*Conexão com banco de dados
+* @author João Paulo
+* @version 1.0
+*/
 public class ModuloConexao {
-   // o metodo abaixo faz a conexao com o banco de dados
+   /**
+    * Metodo responsavel pela conexão com o banco de dados
+    * @return conexao
+    */
    public static Connection conector(){
        Connection conexao = null; 
        // a linha abaixo chama o drive de coneção com o banco de dados
